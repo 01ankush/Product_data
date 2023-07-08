9a756d8112a0a2a875f7797d2e4d9f5dd12a72dd
Ankush kumar kothiyal <82761600+01ankush@users.noreply.github.com>
2023-07-08 15:25:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,36 @@
-const express = require("express");
-const users = require('./db.json');
-const productusers = require('./db_id.json');
-const app = express();
-const PORT = process.env.PORT || 8000;
-const cors = require('cors');
-const corsOptions ={
-    origin:'http://localhost:3000', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
-app.use(cors(corsOptions));
-// app.use(express.json());
+const jsonServer = require("json-server"); // importing json-server library
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
 
-app.get('/api/products',(req,res)=>{
-    return res.json(users);
-})
+server.use(middlewares);
+server.use(router);
 
-app.get('/api/products/:id',(req,res)=>{
-    const id = req.params.id;
-    const user = productusers.find((user) => user.id === id);
-    return res.json(user);
-})
+server.listen(port);
+// const express = require("express");
+// const users = require('./db.json');
+// const productusers = require('./db_id.json');
+// const app = express();
+// const PORT = process.env.PORT || 8000;
+// const cors = require('cors');
+// const corsOptions ={
+//     origin:'http://localhost:3000', 
+//     credentials:true,            //access-control-allow-credentials:true
+//     optionSuccessStatus:200
+// }
+// app.use(cors(corsOptions));
+// // app.use(express.json());
+
+// app.get('/api/products',(req,res)=>{
+//     return res.json(users);
+// })
+
+// app.get('/api/products/:id',(req,res)=>{
+//     const id = req.params.id;
+//     const user = productusers.find((user) => user.id === id);
+//     return res.json(user);
+// })
 
 
 
