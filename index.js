adb6d5e8b2cdc1dc5ac4e38127a5740bd5356414
Ankush kumar kothiyal <82761600+01ankush@users.noreply.github.com>
2023-07-08 15:28:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const jsonServer = require("json-server"); // importing json-server library
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const router1 = jsonServer.router("db.json");
+const router = jsonServer.router("db_id.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
 
 server.use(middlewares);
+server.use(router1);
 server.use(router);
-
 server.listen(port);
 // const express = require("express");
 // const users = require('./db.json');
